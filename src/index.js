ccf1aaf519bc4d46fbb1c05946033f7d3c2a474b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-04-29 21:05:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,25 @@ const client = new Discord.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const prefix = "#";
+
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
     console.log("Bot is ready :3 ");
+});
+
+client.on("messageCreate", (msg) => {
+    if(!msg.guild) return;
+    if(!msg.content.startsWith(prefix)) return;
+    if(msg.content === prefix + "Hello"){
+        msg.reply({
+            content: "Okay",
+        });
+    }
+    if(msg.content === prefix + "Bom dia") {
+        msg.reply({
+            content: "Bom dia, " + msg.author.username,
+        });
+    }
 });
