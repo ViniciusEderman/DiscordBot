5c493d41ebdc65b8206bc09bd64cdc02577e6cb3
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-04-30 14:39:37 -0300

    Bot de música finalizado!

--- src/commands.js
+++ src/commands.js
@@ -1,35 +1,56 @@
-const {player} = require(".")
+const { player } = require(".");
 
-module.exports = async (cliente, msg, args, command) => {
-    if(command === 'tocar'){
-        const channel = msg.member.voice.channel;
-        if(!channel) return msg.channel.send("Você precisa entrar no canal de voz");
+module.exports = async (client, msg, args, command) => {
+  if (command === "play") {
+    const channel = msg.member.voice.channel;
+    if (!channel)
+      return msg.channel.send("Para iniciarmos, preciso que você entre em algum canal de voz");
 
-        const search_music = args.join("");
-        if(!search_music) return msg.channel.send("Digite o nome da música ou o link:");
+    const search_music = args.join(" ");
+    if (!search_music)
+      return msg.channel.send("Digite o nome/link da musica: ");
 
-        const queue =  player.createQueue(msg.guild.id, {
-            metadata: {
-                channel: msg.channel,
-            },
-        });
-        try {
-            if(!queue.connection) await queue.connect(channel);
-        } catch (error) {
-            queue.destroy()
-            return await msg.reply({
-                content: "não foi possível no servidor",
-                ephemeral: true, 
-            });
-        }
-        const song = await player.search(search_music, {
-            requestedBy:msg.author,
-        }).then((x) =>x.tracks[0])
-        client.user.setActivity(song.title, { type: "LISTENING" });
-        if(!song) return msg.reply('erro ao procurar música:  ${search_music} ');
-        queue.play(song);
+    const queue = player.createQueue(msg.guild.id, {
+      metadata: {
+        channel: msg.channel,
+      },
+    });
 
-        msg.channel.send({content: 'buscando ${song.title}'});
-    }else if(command === "skip"){
+    try {
+      if (!queue.connection) await queue.connect(channel);
+    } catch (error) {
+      queue.destroy();
+      return await msg.reply({
+        content: "Não foi possivel entrar no server!!",
+        ephemeral: true,
+      });
     }
+
+    const song = await player
+      .search(search_music, {
+        requestedBy: msg.author,
+      })
+      .then((x) => x.tracks[0]);
+    client.user.setActivity(song.title, { type: "LISTENING" });
+    if (!song) return msg.reply(`Erro na busca da música: ${search_music}!!!`);
+    queue.play(song);
+
+    msg.channel.send({ content: `Buscando por: **${song.title}**!` });
+  } else if (command === "skip") {
+    const queue = player.getQueue(msg.guild.id);
+    queue.skip();
+    msg.channel.send(`Proxima música.`);
+  } else if (command === "next") {
+    const queue = player.getQueue(msg.guild.id);
+    queue.stop();
+    msg.channel.send(`Pediu pra parar parouuu.`);
+  } else if (command === "pause") {
+    const queue = player.getQueue(msg.guild.id);
+    queue.setPaused(true);
+    msg.channel.send(`Pause.`);
+  } else if (command === "continue") {
+    const queue = player.getQueue(msg.guild.id);
+    queue.setPaused(false);
+    msg.channel.send(`Continuando a tocar.`);
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,71 +1,39 @@
-const Discord = require('discord.js');
-const { Intents } = require('discord.js');
-const dotenv = require('dotenv');
-const { Client } = require('discord.js');
-const { Player } = require('discord-player');
-const {prefix, token} = require("../config.json")
+const { Client } = require("discord.js");
+const { Player } = require("discord-player");
+const { prefix, token } = require("../config.json");
 
-
-dotenv.config();
-
-const client = new Discord.Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+const client = new Client({
+  restTimeOffset: 0,
+  shards: "auto",
+  intents: 641,
 });
 
-
-
-client.login(process.env.TOKEN);
-
-client.on("ready", () => {
-    console.log("Bot is ready :3 ");
-});
-
-client.on("messageCreate", (msg) => {
-    if(!msg.guild) return;
-    if(!msg.content.startsWith(prefix)) return;
-    if(msg.content === prefix + "Hello"){
-        msg.reply({
-            content: "Okay",
-        });
-    }
-    if(msg.content === prefix + "Bom dia") {
-        msg.reply({
-            content: "Bom dia, " + msg.author.username,
-        });
-    }
-});
-
-const cliente = new Client({
-    restTimeOffset: 0,
-    shards: "auto",
-    intents: 641,
-});
-
-const player = new Player(cliente, {
-    leaveOnEnd: true,
-    leaveOnStrop: true,
-    leaveOnEmpty: true,
-    leaveOnEmptyCooldown: 5000, 
-    autoSelfDeaf: true,
-    initialVolume: 50,
-    bufferingTimeout: 3000
+const player = new Player(client, {
+  leaveOnEnd: true,
+  leaveOnStop: true,
+  leaveOnEmpty: true,
+  leaveOnEmptyCooldown: 5000,
+  autoSelfDeaf: true,
+  initialVolume: 50,
+  bufferingTimeout: 3000,
 });
 
 client.on("ready", () => {
-    console.log("bot ativo");
-    client.user.setActivity("Your Song", { type: "LISTENING" });
-}); 
-
-client.on("messageCreate", (msg) => {
-    if(!msg.guild || msg.author.bot) return;
-    if(!msg.content.startsWith(prefix)) return;
-
-    const args = msg.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    require("./commands")(client, msg, args, command);
+  console.log("bot is already activated 🤖");
+  client.user.setActivity("Your Song", { type: "LISTENING" });
 });
 
-module.exports = { player, cliente};
+module.exports = { player, client };
+require("./envents")(client);
 
-client.login()
+client.on("messageCreate", (msg) => {
+  if (!msg.guild || msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  require("./commands")(client, msg, args, command);
+});
+
+client.login(token);
