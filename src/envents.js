f97d8c4b91ee7173998f700b232a8c672ba76de6
Vinicius Ederman Magalhães Coelho <87389323+ViniciusEderman@users.noreply.github.com>
2022-09-16 20:58:25 -0300

    Update clean code

--- src/envents.js
+++ src/envents.js
@@ -4,6 +4,7 @@ module.exports = async () => {
   player.on("trackStart", async (queue, track) => {
     queue.metadata.channel.send(`🎵 Tocando \`${track.title}\``);
   });
+  
   player.on("trackAdd", async (queue, track) => {
     queue.metadata.channel.send(`🎵 Adicionando a playlist \`${track.title}\``);
   });
